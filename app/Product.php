a5ec8c8a097c15e48bd2402f7f93491f6831b7bf
mzwae <mrmonjaie@gmail.com>
2020-05-19 17:34:27 +0100

    Update product model

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $table = 'products';
+
+    protected $fillable = ['seller_id', 'upc', 'name', 'price', 'image', 'description'];
+
+    public function seller(){
+        return $this->belongsTo(User::class, 'seller_id');
+    }
 }
