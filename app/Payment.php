b5d4e47f17eed425fe9f375585f5365068fe3456
mzwae <mrmonjaie@gmail.com>
2020-05-20 12:25:51 +0100

    Add fillables and relationships to payment model

--- app/Payment.php
+++ app/Payment.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    protected $table = 'payments';
+
+    protected $fillable = ['customer_id', 'product_id', 'stripe_charge_id', 'paid_out', 'fees_collected', 'refunded'];
+
+    protected $casts = ['refunded' => 'boolean'];
+
+    public function customer()
+    {
+        return $this->hasOne(User::class, 'customer_id');
+    }
+
+    public function product()
+    {
+        return $this->hasOne(User::class, 'product_id');
+    }
 }
