1c7e1d627805704da03dbc2f03c98c4b8a4976a8
mzwae <mrmonjaie@gmail.com>
2020-05-25 22:49:44 +0100

    Add customer controller

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\Stripe\Customer;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Stripe\Token;
+
+class CustomerController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function form()
+    {
+        return view('stripe.form');
+    }
+
+    public function save(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'cc_number' => 'required',
+            'month' => 'required',
+            'year' => 'required',
+            'cvv' => 'required'
+        ]);
+
+        $card = [
+            'card' => [
+                'number' => $request->cc_number,
+                'exp_month' => $request->month,
+                'exp_year' => $request->year,
+                'cvc' => $request->cvv,
+            ]
+        ];
+
+        $user = Auth::user();
+        Customer::save($user, $card);
+        return redirect()->route('products')->with('success', 'Card has been saved.');
+    }
+}
