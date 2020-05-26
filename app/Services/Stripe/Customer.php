f440edfd265f68e774e083761f0616fc4d43aada
mzwae <mrmonjaie@gmail.com>
2020-05-26 15:54:53 +0100

    Add customer service

---
+++ app/Services/Stripe/Customer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Services\Stripe;
+
+use App\User;
+use Stripe\Stripe;
+use Stripe\Token;
+
+class Customer
+{
+    public static function save(User $user, array $card)
+    {
+        Stripe::setApiKey(config('services.stripe.secret'));
+
+        $token = Token::create($card);
+        $customer = \Stripe\Customer::create([
+            'source' => $token->id,
+            'email' => $user->email
+        ]);
+
+        $user->update(
+            [
+                'stripe_customer_id' => $customer->id
+            ]
+        );
+    }
+}
