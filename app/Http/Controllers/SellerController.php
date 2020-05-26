e7455cd28dce2260ed9e26f16f1ef2923d6665c5
mzwae <mrmonjaie@gmail.com>
2020-05-26 11:49:02 +0100

    Add seller ctrl

---
+++ app/Http/Controllers/SellerController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SellerController extends Controller
+{
+    public function create()
+    {
+        $user = Auth::user();
+        if (!is_null($use->stripe_connect_id)) {
+            return redirect()->route('stripe.login');
+        }
+        $session = request()->session()->getId();
+        $url = config('services.stripe.connect') . $session;
+        return redirect($url);
+    }
+}
