7381ed7508bfc4a3ffcbaadc2fd7e3afc2ae45e9
mzwae <mrmonjaie@gmail.com>
2020-05-28 17:40:08 +0100

    Pay funds and save transaction

--- app/Services/Stripe/Transaction.php
+++ app/Services/Stripe/Transaction.php
@@ -19,11 +19,31 @@ class Transaction
         Stripe::setApiKey(config('services.stripe.secret'));
 
         // Create a Stripe charge from the customer purchase
+        $charge = Charge::create([
+            'amount' => self::toStripeFormat($amount),
+            'currency' => 'usd',
+            'customer' => $user->stripe_customer_id,
+            'description' => $product->name
+        ]);
 
 
         // Pay funds to seller, with platform fees extracted
+        Transfer::create([
+            'amount' => self::toStripeFormat($payout),
+            'currency' => 'usd',
+            'source_transaction' => $charge->id,
+            'destination' => $product->seller->stripe_connect_id
+        ]);
 
 
         // Save transaction to database
+        $payment = new Payment();
+        $payment->customer_id = $user->id;
+        $payment->product_id = $product->id;
+        $payment->stripe_charge_id = $charge->id;
+        $payment->paid_out = $payout;
+        $payment->fees_collected = $amount - $payout;
+        $payment->save();
     }
+    
 }
