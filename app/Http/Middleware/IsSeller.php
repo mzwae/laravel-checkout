a1115e6741697a88762eb1a60f46902df9360c23
mzwae <mrmonjaie@gmail.com>
2020-05-30 10:15:06 +0100

    Add IsSeller middleware

---
+++ app/Http/Middleware/IsSeller.php
@@ -0,0 +1,25 @@
+  
+<?php
+
+namespace App\Http\Middleware;
+
+use App\User;
+use Closure;
+
+class IsSeller
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(auth()->user()->type != User::SELLER){
+            return redirect()->route('products')->with('error', 'You are not a seller.');
+        }
+        return $next($request);
+    }
+}
