e21b946da254e44b11cca243a379ca748d73904e
mzwae <mrmonjaie@gmail.com>
2020-05-30 16:51:58 +0100

    Add Stripe redirect methods

--- app/Http/Controllers/SellerController.php
+++ app/Http/Controllers/SellerController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\Stripe\Seller;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Stripe\Stripe;
+use Stripe\Account;
 
 class SellerController extends Controller
 {
@@ -15,6 +20,37 @@ class SellerController extends Controller
         }
         $session = request()->session()->getId();
         $url = config('services.stripe.connect') . $session;
+
+        // Redirect Stripe to create account
         return redirect($url);
     }
+
+
+    public function login()
+    {
+        $user = Auth::user();
+        Stripe::setApiKey(config('services.stripe.secret'));
+        $account_link = Account::createLoginLink($user->stripe_connect_id);
+
+        // Redirect Stripe to Connect Account
+        return redirect($account_link->url);
+    }
+
+
+    // Save a Stripe Connect Account
+    public function save(Request $request)
+    {
+        $this->validate($request, [
+            'code' => 'required',
+            'state' => 'required'
+        ]);
+
+        $session = DB::table('session')->where('id', $request->state)->first();
+        $data = Seller::create($request->code);
+        User::find($session->user_id)->update(['stripe_connect_id' => $data->stripe_user_id]);
+        return redirect()->route('products')->with('success', 'Account information has been saved.');
+
+    }
+
+
 }
